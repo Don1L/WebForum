74f0e5627db34671ef160c8d1aa753e6dc99f019
Don1L <danilic206@gmail.com>
2025-10-16 14:45:31 +0300

    Creating a ViewModel for a CommentController

---
+++ ViewModels/EditCommentViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebForum.ViewModels;
+
+public class EditCommentViewModel
+{
+    [Required]
+    [StringLength(500, ErrorMessage = "The {0} must be at max {1} characters long."), MaxLength(500)]
+    public string Text { get; set; } =  string.Empty;
+    
+    [Required]
+    [StringLength(500, ErrorMessage = "The {0} must be at max {1} characters long."), MaxLength(500)]
+    public string? PhotoPath { get; set; }
+    
+    //Id поста к которому будет написан комментарий
+    public int PostId { get; set; }
+    
+    //Id треда в котором будет наш пост
+    public int ThreadId { get; set; }
+}
