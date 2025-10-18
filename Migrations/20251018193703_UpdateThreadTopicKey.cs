b136230735dbf44407bb4545ff9da9e8d9361e11
Don1L <danilic206@gmail.com>
2025-10-18 22:47:20 +0300

    Update DB

---
+++ Migrations/20251018193703_UpdateThreadTopicKey.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateThreadTopicKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ThreadTopics_Threads_TheadId",
+                table: "ThreadTopics");
+
+            migrationBuilder.RenameColumn(
+                name: "TheadId",
+                table: "ThreadTopics",
+                newName: "ThreadId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ThreadTopics_Threads_ThreadId",
+                table: "ThreadTopics",
+                column: "ThreadId",
+                principalTable: "Threads",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ThreadTopics_Threads_ThreadId",
+                table: "ThreadTopics");
+
+            migrationBuilder.RenameColumn(
+                name: "ThreadId",
+                table: "ThreadTopics",
+                newName: "TheadId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ThreadTopics_Threads_TheadId",
+                table: "ThreadTopics",
+                column: "TheadId",
+                principalTable: "Threads",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
